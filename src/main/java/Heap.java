cbed5aa94ca7dd2378cfbc2dbb3320ccd77b72d4
Zagidullin Ayrat <zagidull007@gmail.ru>
2023-09-17 01:21:00 +0300

    Heap - добавил реализацию кучи на основе дерева с массивом

---
+++ src/main/java/Heap.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+class Heap
+{
+    public int [] HeapArray; // хранит неотрицательные числа-ключи
+
+    public Heap() { HeapArray = null; }
+
+    public void MakeHeap(int[] a, int depth)
+    {
+        int heapSize = (int) Math.pow(2, (depth + 1)) - 1;
+        HeapArray = new int[heapSize];
+
+        for (int key: a) {
+            Add(key);
+        }
+        // создаём массив кучи HeapArray из заданного
+        // размер массива выбираем на основе глубины depth
+        // ...
+    }
+
+    public int GetMax()
+    {
+        if (HeapArray == null || HeapArray[0] == 0)
+            return -1;
+        int lastFilledIndex = findLastFilledIndex();
+        HeapArray[0] = HeapArray[lastFilledIndex];
+        HeapArray[lastFilledIndex] = 0;
+        moveElementToDown(0);
+        // вернуть значение корня и перестроить кучу
+        return HeapArray[0]; // если куча пуста
+    }
+
+    public boolean Add(int key)
+    {
+        if (HeapArray == null || HeapArray[HeapArray.length - 1] != 0)
+            return false;
+        int firstEmptyIndex = findFirstEmptyIndex();
+        HeapArray[firstEmptyIndex] = key;
+        moveElementToTop(firstEmptyIndex);
+
+        // добавляем новый элемент key в кучу и перестраиваем её
+        return true; // если куча вся заполнена
+    }
+
+    private int findFirstEmptyIndex() {
+        for (int i = 0; i < HeapArray.length; i++) {
+            if (HeapArray[i] == 0)
+                return i;
+        }
+        return -1;
+    }
+
+    private int findLastFilledIndex() {
+        for (int i = HeapArray.length - 1; i >= 0; i--) {
+            if (HeapArray[i] != 0)
+                return i;
+        }
+        return -1;
+    }
+
+    private void moveElementToTop(int currentIndex) {
+        if (currentIndex == 0)
+            return;
+        boolean isRightElementIndex = currentIndex % 2 == 0;
+        int currentRootIndex = (currentIndex - (isRightElementIndex ? 2 : 1)) / 2;
+
+        if (HeapArray[currentRootIndex] > HeapArray[currentIndex])
+            return;
+
+        int temp = HeapArray[currentRootIndex];
+        HeapArray[currentRootIndex] = HeapArray[currentIndex];
+        HeapArray[currentIndex] = temp;
+        moveElementToTop(currentRootIndex);
+    }
+
+    private void moveElementToDown(int currentIndex) {
+        int leftElementIndex = 2 * currentIndex + 1;
+        int rightElementIndex = 2 * currentIndex + 2;
+
+        if (HeapArray[currentIndex] > HeapArray[leftElementIndex] && HeapArray[currentIndex] > HeapArray[rightElementIndex])
+            return;
+
+        int maxNextElementIndex = HeapArray[leftElementIndex] > HeapArray[rightElementIndex] ? leftElementIndex : rightElementIndex;
+        int temp = HeapArray[currentIndex];
+        HeapArray[currentIndex] = HeapArray[maxNextElementIndex];
+        HeapArray[maxNextElementIndex] = temp;
+        moveElementToDown(maxNextElementIndex);
+    }
+
+}
---
+++ src/test/java/HeapTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapTest {
+
+    private final Heap heap = new Heap();
+
+    @Test
+    public void getMaxWhenHeapIsEmpty() {
+        assertEquals(-1, heap.GetMax());
+    }
+    
+    @Test
+    public void getMaxWhenHeapIsNotEmpty() {
+        int[] arr = new int[] {12, 5, 13, 6, 14, 7, 15, 8, 11, 9, 10, 4, 2, 1, 3};
+        heap.MakeHeap(arr, 4);
+
+        assertEquals(14, heap.GetMax());
+    }
+
+    @Test
+    public void MakeHeapTest() {
+        int[] arr = new int[] {12, 5, 13, 6, 14, 7, 15, 8, 11, 9, 10, 4, 2, 1, 3};
+        heap.MakeHeap(arr, 3);
+
+        assertEquals(15, heap.HeapArray.length);
+
+        assertEquals(15, heap.HeapArray[0]);
+        assertEquals(13, heap.HeapArray[1]);
+        assertEquals(14, heap.HeapArray[2]);
+        assertEquals(11, heap.HeapArray[3]);
+        assertEquals(10, heap.HeapArray[4]);
+        assertEquals(7, heap.HeapArray[5]);
+        assertEquals(12, heap.HeapArray[6]);
+        assertEquals(5, heap.HeapArray[7]);
+        assertEquals(8, heap.HeapArray[8]);
+        assertEquals(6, heap.HeapArray[9]);
+        assertEquals(9, heap.HeapArray[10]);
+        assertEquals(4, heap.HeapArray[11]);
+        assertEquals(2, heap.HeapArray[12]);
+        assertEquals(1, heap.HeapArray[13]);
+        assertEquals(3, heap.HeapArray[14]);
+    }
+
+    @Test
+    public void AddTestWhenHeapWhenHeapFilled() {
+        int[] arr = new int[] {12, 5, 13, 6, 14, 7, 15, 8, 11, 9, 10, 4, 2, 1, 3};
+        heap.MakeHeap(arr, 3);
+
+        assertFalse(heap.Add(88));
+    }
+
+    @Test
+    public void AddTestWhenHeapWhenHeapIsNotFilled() {
+        int[] arr = new int[] {12, 5, 13, 6, 14, 7, 15, 8, 11, 9, 10, 4, 2, 1};
+        heap.MakeHeap(arr, 3);
+
+        assertTrue(heap.Add(3));
+        assertEquals(3, heap.HeapArray[14]);
+    }
+}
