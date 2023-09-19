c7c28444a0c8b4cb53b420428a80d2103cbea7bd
Zagidullin Ayrat <zagidull007@gmail.ru>
2023-09-19 13:54:40 +0300

    Heap - дополнил тесты

--- src/test/java/HeapTest.java
+++ src/test/java/HeapTest.java
@@ -17,53 +17,91 @@ class HeapTest {
         assertEquals(0, heap.GetMax());
     }
 
-
     @Test
     public void getMaxWhenHeapIsNotEmpty() {
-        int[] arr = new int[] {12, 5, 13, 6, 14, 7, 15, 8, 11, 9, 10, 4, 2, 1, 3};
+        int[] arr = new int[] {12, 45, 23, 66, 4, 47, 50, 81, 111, 95, 120, 41, 28, 19, 92};
         heap.MakeHeap(arr, 4);
 
-        assertEquals(14, heap.GetMax());
+        assertEquals(111, heap.GetMax());
     }
 
     @Test
     public void MakeHeapTest() {
-        int[] arr = new int[] {12, 5, 13, 6, 14, 7, 15, 8, 11, 9, 10, 4, 2, 1, 3};
+        int[] arr = new int[] {12, 45, 23, 66, 4, 47, 50, 81, 111, 95, 120, 41, 28, 19, 92};
         heap.MakeHeap(arr, 3);
 
         assertEquals(15, heap.HeapArray.length);
 
-        assertEquals(15, heap.HeapArray[0]);
-        assertEquals(13, heap.HeapArray[1]);
-        assertEquals(14, heap.HeapArray[2]);
-        assertEquals(11, heap.HeapArray[3]);
-        assertEquals(10, heap.HeapArray[4]);
-        assertEquals(7, heap.HeapArray[5]);
-        assertEquals(12, heap.HeapArray[6]);
-        assertEquals(5, heap.HeapArray[7]);
-        assertEquals(8, heap.HeapArray[8]);
-        assertEquals(6, heap.HeapArray[9]);
-        assertEquals(9, heap.HeapArray[10]);
-        assertEquals(4, heap.HeapArray[11]);
-        assertEquals(2, heap.HeapArray[12]);
-        assertEquals(1, heap.HeapArray[13]);
-        assertEquals(3, heap.HeapArray[14]);
+        assertEquals(120, heap.HeapArray[0]);
+        assertEquals(111, heap.HeapArray[1]);
+        assertEquals(92, heap.HeapArray[2]);
+        assertEquals(66, heap.HeapArray[3]);
+        assertEquals(95, heap.HeapArray[4]);
+        assertEquals(41, heap.HeapArray[5]);
+        assertEquals(50, heap.HeapArray[6]);
+        assertEquals(12, heap.HeapArray[7]);
+        assertEquals(45, heap.HeapArray[8]);
+        assertEquals(4, heap.HeapArray[9]);
+        assertEquals(81, heap.HeapArray[10]);
+        assertEquals(23, heap.HeapArray[11]);
+        assertEquals(28, heap.HeapArray[12]);
+        assertEquals(19, heap.HeapArray[13]);
+        assertEquals(47, heap.HeapArray[14]);
     }
 
     @Test
     public void AddTestWhenHeapWhenHeapFilled() {
-        int[] arr = new int[] {12, 5, 13, 6, 14, 7, 15, 8, 11, 9, 10, 4, 2, 1, 3};
+        int[] arr = new int[] {12, 45, 23, 66, 4, 47, 50, 81, 111, 95, 120, 41, 28, 19, 92};
         heap.MakeHeap(arr, 3);
 
         assertFalse(heap.Add(88));
     }
 
     @Test
-    public void AddTestWhenHeapWhenHeapIsNotFilled() {
-        int[] arr = new int[] {12, 5, 13, 6, 14, 7, 15, 8, 11, 9, 10, 4, 2, 1};
+    public void AddTestWhenHeapWhenHeapIsNotFilledAndKeyAddInMiddle() {
+        int[] arr = new int[] {12, 45, 23, 66, 4, 47, 50, 81, 111, 95, 120, 41, 28, 19};
         heap.MakeHeap(arr, 3);
 
-        assertTrue(heap.Add(3));
-        assertEquals(3, heap.HeapArray[14]);
+        assertTrue(heap.Add(110));
+        assertEquals(110, heap.HeapArray[2]);
+        assertEquals(41, heap.HeapArray[5]);
+        assertEquals(50, heap.HeapArray[6]);
+        assertEquals(23, heap.HeapArray[11]);
+        assertEquals(28, heap.HeapArray[12]);
+        assertEquals(19, heap.HeapArray[13]);
+        assertEquals(47, heap.HeapArray[14]);
+    }
+
+    @Test
+    public void AddTestWhenHeapWhenHeapIsNotFilledAndKeyAddInRoot() {
+        int[] arr = new int[] {12, 45, 23, 66, 4, 47, 50, 81, 111, 95, 120, 41, 28, 19};
+        heap.MakeHeap(arr, 3);
+
+        assertTrue(heap.Add(150));
+        assertEquals(150, heap.HeapArray[0]);
+        assertEquals(120, heap.HeapArray[2]);
+        assertEquals(41, heap.HeapArray[5]);
+        assertEquals(50, heap.HeapArray[6]);
+        assertEquals(23, heap.HeapArray[11]);
+        assertEquals(28, heap.HeapArray[12]);
+        assertEquals(19, heap.HeapArray[13]);
+        assertEquals(47, heap.HeapArray[14]);
+    }
+
+    @Test
+    public void AddTestTwoWhenHeapWhenHeapIsNotFilledAndKeyAddInRoot() {
+        int[] arr = new int[] {12, 45, 23, 66, 4, 47, 50, 81};
+        heap.MakeHeap(arr, 3);
+
+        assertTrue(heap.Add(150));
+        assertEquals(150, heap.HeapArray[0]);
+        assertEquals(81, heap.HeapArray[1]);
+        assertEquals(50, heap.HeapArray[2]);
+        assertEquals(66, heap.HeapArray[3]);
+        assertEquals(4, heap.HeapArray[4]);
+        assertEquals(23, heap.HeapArray[5]);
+        assertEquals(47, heap.HeapArray[6]);
+        assertEquals(12, heap.HeapArray[7]);
+        assertEquals(45, heap.HeapArray[8]);
     }
 }
